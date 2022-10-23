f316269dd3613600d729d506c206c00395e61e68
Lika Maiorova <kwinyoku@gmail.com>
2022-10-23 17:57:05 +0300

    задача на угадывание чисел

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+
+/*
+компьютер должен загадать число от 0 до 9
+пользователю нужно угодать число
+в программе должен вестись подсчет количества попыток угадывания
+ */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int userNum;
+        int count = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Задача: угадай число от 0 до 9");
+
+        int num = (int)Math.floor(Math.random() * 10);
+
+        do {
+            count++;
+            System.out.println("введите число");
+            userNum = input.nextInt();
+
+            if (userNum > num) {
+                System.out.println(" мое число меньше");
+            } else if (userNum < num) {
+                System.out.println(" мое число больше");
+            } else {
+                System.out.println(" вы угадали мое число - " + userNum);
+            }
+        } while (userNum != num);
+
+        System.out.println("число попыток " + count);
     }
-}
+ }
